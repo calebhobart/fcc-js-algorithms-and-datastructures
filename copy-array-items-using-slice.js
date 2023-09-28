39eee5122992bb35aced3adee0ad2e564af1742b
Caleb Hobart <hobart.caleb@gmail.com>
2023-09-28 09:05:02 -0400

    9/27 lessons

---
+++ copy-array-items-using-slice.js
@@ -0,0 +1,9 @@
+function forecast(arr) {
+  // Only change code below this line
+  let sub_cast = arr.slice(2,4);
+  
+  return sub_cast;
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
